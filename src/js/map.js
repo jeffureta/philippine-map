6f511b021c06c94dda9836ce41bbb7d6eec32956
jeffureta <jeffvincentureta@gmail.com>
2025-12-06 18:09:50 +0000

    Use ph_updated_nir.json as geoJSON data

--- src/js/map.js
+++ src/js/map.js
@@ -5,7 +5,7 @@ export const map = new maplibregl.Map({
         sources: {
             'philippines': {
                 type: 'geojson',
-                data: 'src/data/ph.json' // Path to your GeoJSON file
+                data: 'src/data/ph_updated_nir.json' // Path to your GeoJSON file
             }
         },
         layers: [
